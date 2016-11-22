7586c094d44dbdf4aa1a57e9bae9ddea5b2cd299
François Leurent <fleurent@ivsweb.com>
2016-11-22 09:24:32 +0100

    Allow to work with an existing server instance

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const fs      = require('fs');
+const path    = require('path');
 const defer   = require('nyks/promise/defer');
 const sleep   = require('nyks/function/sleep');
 
@@ -23,40 +24,39 @@ class Server {
 
     var lnk = new pg(this.config.admin);
 
-    lnk.on('error', function(err){
+      //forget this and you'll crash your main loop
+    lnk.on('error', function(err) {
       console.log("FAILURE IN sql client link");
     });
 
+      //maybe the server is already running (!)
+    try {
+      yield  lnk.connect();
+      console.log("Connected to an existing pg server instance");
+    } catch(err) {
+      let defered = defer();
 
-    var self = this;
+      server(this.config.admin.dataDir, defered.chain);
+      var instance = yield defered;
 
-    var defered = defer();
-
-    server(this.config.admin.dataDir, function(err, instance){
       instance.stderr.pipe(process.stderr);
       instance.stdout.pipe(process.stdout);
-      self.instance = instance;
+
+      this.instance = instance;
+
+      console.log("Server is running, trying to connect");
+      var tries = 5;
 
 
-      defered.resolve();
-    });
+      while(true) {
+        if(!tries --)
+          throw "Could not connect";
 
-    yield defered;
-
-
-
-    console.log("Server is running, trying to connect");
-    var tries = 5;
-
-
-    while(true) {
-      if(!tries --)
-        throw "Could not connect";
-
-      try {
-        yield  lnk.connect();
-        break;
-      } catch(err) { yield sleep(1500); }
+        try {
+          yield  lnk.connect();
+          break;
+        } catch(err) { yield sleep(1500); }
+      }
     }
 
     //now that the server is ready, check for ivscsac database
@@ -82,14 +82,20 @@ class Server {
   }
 
   * populate() {
-    var psql_path = "node_modules/pg-server-9.5-win-x86/server/bin/psql";
-    var mock_data = './test/mock/ta.sql';
-    yield passthru(psql_path, ["-U", "postgres",  "-f", mock_data, this.config.dbname])
+    try {
+      var psql_path = path.resolve(__dirname, "node_modules/pg-server-9.5-win-x86/server/bin/psql.exe");
+      var mock_data = './test/mock/ta.sql';
+      yield passthru(psql_path, ["-U", "postgres",  "-f", mock_data, this.config.dbname])
+    } catch(err) {
+      console.error("Could not populate db with mock data", err);
+    }
   }
 
-  stop(){
-    this.instance.softkill();
-    console.log("Server killed, bye");
+  stop() {
+    if(this.instance) {
+      this.instance.softkill();
+      console.log("Server killed, bye");
+    }
 
     setTimeout(function(){
       process.exit();
